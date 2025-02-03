9eafa4ea9e71100e1a0e4ad7d68728789d64900e
May Sunktong <pattaranan.sunktong@gmail.com>
2025-02-03 21:59:36 +0100

    Implement all departures for a randomed station

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(() => {
-  const API_DEPARTURES = "https://transport.integration.sl.se/v1/sites";
+  const BASE_API = "https://transport.integration.sl.se/v1/sites";
   const getRandomStation = async () => {
     try {
       let hasDepartures = false;
       let data;
-      /* Random station must have at least 1 departure */
+
       while (!hasDepartures) {
         let randomId = Math.floor(Math.random() * (9999 - 100 + 1)) + 100;
-        let response = await fetch(`${API_DEPARTURES}/${randomId}/departures`);
+        let response = await fetch(`${BASE_API}/${randomId}/departures`);
         if (!response.ok) {
           console.log(
             `Station ID ${randomId} not found or error fetching data.`
@@ -16,22 +16,20 @@ $(() => {
         }
         data = await response.json();
 
+        /* Random station must have at least 1 departure */
         if (data.departures && data.departures.length > 0) {
           hasDepartures = true;
           console.log("Departures found for station ID:", randomId);
-        } else {
-          console.log("No departures found for station ID:", randomId);
         }
       }
-      $("#randomStationText").html(
+      $("#station-name").html(
         `${data.departures[0]["stop_area"]["name"]}`
       );
-      const sliceArray = data.departures.slice(0, data.departures.length);
-      console.log(sliceArray);
-      const map = sliceArray.map((station) => {
+      const totalDepartures = data.departures.slice(0, data.departures.length);
+      const showDepartures = totalDepartures.map((station) => {
         return station.destination;
       });
-      console.log(map);
+      $('#search-result').html(`${showDepartures}`)
     } catch (error) {
       console.log(error);
     }
