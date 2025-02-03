c74182b6b9a5bf0ecf0957744aaa0a9a72d71854
May Sunktong <pattaranan.sunktong@gmail.com>
2025-02-03 01:32:17 +0100

    Add random station functionality and feature station name

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 $(() => {
   const API_DEPARTURES = "https://transport.integration.sl.se/v1/sites";
-  const showTransport = async () => {
+  const getRandomStation = async () => {
     try {
-      let randomID = Math.floor(Math.random() * 9999);
-      console.log("randomID", randomID);
-      let departuresRes = await fetch(
-        `${API_DEPARTURES}/${9117}/departures`
-      );
-      let departure = await departuresRes.json();
-      console.log("Number of departures:", departure.departures.length);
-      const sliceArray = departure.departures.slice(0,departure.departures.length)
-      console.log(sliceArray)
-      const map = sliceArray.map((station) =>{
-        return station.destination
-      })
-      console.log(map)
+      let hasDepartures = false;
+      let data;
+      /* Random station must have at least 1 departure */
+      while (!hasDepartures) {
+        let randomId = Math.floor(Math.random() * (9999 - 100 + 1)) + 100;
+        let response = await fetch(`${API_DEPARTURES}/${randomId}/departures`);
+        if (!response.ok) {
+          console.log(
+            `Station ID ${randomId} not found or error fetching data.`
+          );
+          continue;
+        }
+        data = await response.json();
 
+        if (data.departures && data.departures.length > 0) {
+          hasDepartures = true;
+          console.log("Departures found for station ID:", randomId);
+        } else {
+          console.log("No departures found for station ID:", randomId);
+        }
+      }
+      $("#randomStationText").html(
+        `${data.departures[0]["stop_area"]["name"]}`
+      );
+      const sliceArray = data.departures.slice(0, data.departures.length);
+      console.log(sliceArray);
+      const map = sliceArray.map((station) => {
+        return station.destination;
+      });
+      console.log(map);
     } catch (error) {
       console.log(error);
     }
   };
-
-  showTransport();
+  $("#randomButton").on("click", getRandomStation);
 });
