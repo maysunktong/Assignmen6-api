a1c4c4e6c060ee64755a02b317cb94327ce251d8
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-02-04 22:37:42 +0100

    Implement express as routing

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs');
+import express from "express";
+import * as path from "path";
 
-const PORT = 3001;
+const PORT = 3010;
+const app = express();
+const __dirname = path.resolve();
+
+const departures = express.Router();
+const deviations = express.Router();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+departures.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/departures.html"));
+});
+
+deviations.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/deviations.html"));
+});
+
+app.use("/departures", departures);
+app.use("/deviations", deviations);
+
+app.listen(PORT, () =>
+  console.log(`✅ Listening on ${PORT} ➡ http://localhost:${PORT}`)
+);
