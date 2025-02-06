bc41306b09257e8791c0472c7ffcaaa65a2ab5d2
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-02-06 22:31:43 +0100

    Delete views

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const __dirname = path.resolve();
 
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
