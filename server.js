b14fcd6b6dee16ebeacaa7eb3a613a3aa5bca562
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-02-06 15:11:09 +0100

    Create a router for /pages/favorites

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as path from "path";
 import departuresRoute from "./routes/departures.js";
 import deviationsRoute from "./routes/deviations.js";
+import favoritesRoute from "./routes/favorites.js";
 
 const PORT = 3010;
 const app = express();
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 
 app.use("/departures", departuresRoute);
 app.use("/deviations", deviationsRoute);
+app.use("/favorites", favoritesRoute);
 
 app.listen(PORT, () =>
   console.log(`✅ Listening on ${PORT} ➡ http://localhost:${PORT}`)
