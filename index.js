51b234eddd6e46f1bc4269fe2612bae2dd896747
May Sunktong <pattaranan.sunktong@gmail.com>
2025-02-10 13:35:27 +0100

    Change home.ejs to index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("pages/home", {
+  res.render("pages/index", {
     title: "Trafikatt",
     subtitle: "Res överallt som en katt",
   });
