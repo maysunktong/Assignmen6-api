1c90187806210c3c70a29c29aadc783776d109d1
May Sunktong <pattaranan.sunktong@gmail.com>
2025-02-10 10:21:20 +0100

    Add back path.join to public

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.resolve();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("pages/index", {
